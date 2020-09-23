8d92622868d5cb16cd5ea5d633a96f8b1f3294bf
CMKawser7476 <69309569+CMKawser7476@users.noreply.github.com>
2020-09-23 17:41:01 +0600

    min & max

---
+++ min_max.sql
@@ -0,0 +1,13 @@
+SELECT MIN(last_worn) 
+FROM shirt_table;
+ 
+SELECT MIN(last_worn) FROM shirt_table;
+ 
+ 
+SELECT MAX(last_worn) 
+FROM shirt_table;
+ 
+
+ 
+SELECT MAX(last_worn), article, description
+FROM shirt_table;
