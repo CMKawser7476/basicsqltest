1783fc0b06568bea23f7399e95424664bc6cd281
CMKawser7476 <69309569+CMKawser7476@users.noreply.github.com>
2020-09-30 17:06:03 +0600

    date data sql

---
+++ date_data.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE people_table (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+ 
+INSERT INTO people_table (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+ 
+INSERT INTO people_table (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+ 
+SELECT * FROM people_table;
