aec048b68d1665968f5f2f219d0374733314e73a
CMKawser7476 <69309569+CMKawser7476@users.noreply.github.com>
2020-09-30 16:18:52 +0600

    float & double sql

---
+++ float_double.sql
@@ -0,0 +1,13 @@
+CREATE TABLE float_table (price FLOAT);
+ 
+INSERT INTO float_table(price) VALUES (88.45);
+ 
+SELECT * FROM float_table;
+ 
+INSERT INTO float_table(price) VALUES (8877.45);
+ 
+SELECT * FROM float_table;
+ 
+INSERT INTO float_table(price) VALUES (8877665544.45);
+ 
+SELECT * FROM float_table;
