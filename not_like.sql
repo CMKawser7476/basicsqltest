f2711e022724b5e502ada955c00393d7b26e77cd
CMKawser7476 <69309569+CMKawser7476@users.noreply.github.com>
2020-10-01 14:14:07 +0600

    not like sql

---
+++ not_like.sql
@@ -0,0 +1,9 @@
+SELECT article FROM shirt_table WHERE article LIKE 't';
+ 
+SELECT article FROM shirt_table WHERE article LIKE 't%';
+ 
+SELECT article FROM shirt_table WHERE article LIKE '%s%';
+ 
+SELECT article FROM shirt_table WHERE article LIKE 's%';
+ 
+SELECT article FROM shirt_table WHERE article NOT LIKE 's%';
