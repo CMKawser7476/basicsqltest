3dbe52957950cb543073db7db546387f7af5ba0b
CMKawser7476 <69309569+CMKawser7476@users.noreply.github.com>
2020-10-01 13:16:22 +0600

    not equal command

---
+++ not_equal.sql
@@ -0,0 +1,9 @@
+SELECT article FROM shirt_table WHERE last_worn = 56;
+ 
+SELECT article, description FROM shirt_table WHERE last_worn != 34;
+ 
+SELECT article, description FROM shirt_table;
+ 
+SELECT article, color FROM shirt_table WHERE article = 't-shirt';
+ 
+SELECT article FROM shirt_table WHERE article != 'polo-shirt';
